5418008fea97c17ff303beceaba5dd3e3b41d514
Nancyiok <161930878+Nancyiok@users.noreply.github.com>
2024-12-15 16:19:17 +0000

    add styles for contact-us section and footer, add burger menu, fixed bugs for phone layout

---
+++ js/index.phone-nav.js
@@ -0,0 +1,50 @@
+const navigationButtonPhone = document.querySelector(
+  ".navigation-menu__phone-nav"
+);
+const navigationMenuPhone = document.querySelector(".navigation-menu--phone");
+const navigationMenuListPhone = document.querySelector(
+  ".navigation-menu__list--phone"
+);
+const navigationMenuLinks = document.querySelectorAll(".navigation-menu__link");
+const navigationMenuListDecorations = document.querySelectorAll(
+  ".navigation-menu__list--decoration"
+);
+const heroSectionContent = document.querySelector(".hero-section__content");
+navigationButtonPhone.addEventListener("click", () => {
+  document.body.style.overflow = "hidden";
+  navigationMenuPhone.classList.toggle("active");
+  navigationMenuListPhone.classList.toggle("active");
+  navigationMenuLinks.forEach((link) => link.classList.toggle("active"));
+  navigationMenuListDecorations.forEach((decoration) =>
+    decoration.classList.toggle("active")
+  );
+  heroSectionContent.classList.toggle("active");
+  navigationButtonPhone.classList.toggle("active");
+});
+
+navigationMenuLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    document.body.style.overflow = "auto";
+  });
+});
+
+const mediaQuery = window.matchMedia("(min-width: 800px)");
+
+function handleMediaQueryChange(e) {
+  if (e.matches) {
+    document.body.style.overflow = "auto";
+    heroSectionContent.classList.remove("active");
+  }
+  document.body.style.overflow = "auto";
+  heroSectionContent.classList.remove("active");
+  navigationMenuPhone.classList.remove("active");
+  navigationMenuListPhone.classList.remove("active");
+  navigationMenuLinks.forEach((link) => link.classList.remove("active"));
+  navigationMenuListDecorations.forEach((decoration) =>
+    decoration.classList.remove("active")
+  );
+  navigationButtonPhone.classList.remove("active");
+}
+
+mediaQuery.addEventListener("change", handleMediaQueryChange);
+handleMediaQueryChange(mediaQuery);
